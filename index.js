3a2f45b59437d7c68dc03d3be0a1e8df7f236dec
zhouHaoZhuang <88356815+zhouHaoZhuang@users.noreply.github.com>
2022-06-29 17:07:46 +0800

    当前就是一个测试

---
+++ index.js
@@ -0,0 +1,7 @@
+import * as data from "./module.js"
+// import name from "./module"
+
+// console.log(name);
+
+console.log(data)
+// Moduledefault: () => "Hello world"name: "c"Symbol(Symbol.toStringTag):
